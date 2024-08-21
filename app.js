a7205b01e06de65acd2df2995fe9715d201319e7
a-jindal <a.jindal@salesforce.com>
2024-08-21 21:49:15 +0530

    adding crd Response view

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 const router = require('./router');
+const cards = require('./data/cards');
+const systemActions = require('./data/systemActions');
 
 app.use('/route', router);
 
@@ -12,10 +14,17 @@ app.set('view engine','ejs');
 app.use('/static',express.static(path.join(__dirname, 'public')));
 app.use('/assets',express.static(path.join(__dirname, 'public/assets')));
 
+
+
 //home route
 app.get('/', function (req, res) {
   res.render('home', {title:"Login System"});
 });
+
+/* to use crdResponse view, use it like this
+app.get('/', function (req, res) {
+  res.render('crdResponse', {title:"CRD Response", cards: cards, systemActions: systemActions});
+});*/
 
 app.listen(port, function () {
   console.log('SF-FHIR-APP listening on port 3000!');
---
+++ data/cards.js
@@ -0,0 +1,28 @@
+const cards = [
+        {
+            "source": {
+                "label": "Payer Label"
+            },
+            "links": {
+                "url": "https://www.cms.gov/Outreach-and-Education/Medicare-Learning-Network-MLN/MLNProducts/Downloads/Home-Oxygen-Therapy-ICN908804.pdf",
+                "type": "absolute",
+                "label": "Home Oxygen Therapy Guidelines"
+            },
+            "summary": "Prior Authorization : Prior Authorization Needed, Document Needed : Clinical Documentation",
+            "indicator": "warning"
+        },
+        {
+            "source": {
+                "label": "Payer Label"
+            },
+            "links": {
+                "url": "https://www.cms.gov/Outreach-and-Education/Medicare-Learning-Network-MLN/MLNProducts/Downloads/Home-Oxygen-Therapy-ICN908804.pdf",
+                "type": "absolute",
+                "label": "Home Oxygen Therapy Guidelines"
+            },
+            "summary": "Prior Authorization : Prior Authorization Needed, Document Needed : Clinical Documentation",
+            "indicator": "warning"
+        }
+    ];
+
+    module.exports = cards;
