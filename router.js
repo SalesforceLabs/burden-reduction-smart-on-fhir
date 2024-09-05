19a6762b848101ae6e4b05f899059c4cf4e01df5
csarma <csarma@salesforce.com>
2024-09-05 12:45:31 +0530

    integ

--- router.js
+++ router.js
@@ -79,10 +79,12 @@ router.post('/updateConfig', (req, res) => {
 });
 
 router.post('/call-ip', async (req, res) => {
-    const {instanceUrl,accessToken, ipType, ipSubtype}  = fileUpdater.getFile(payerConfigFilePath,['instanceUrl','accessToken', 'ipType', 'ipSubtype'])
+    const {instanceUrl,accessToken, ipType, ipSubtype}  = fileUpdater.getFile(payerConfigFilePath,['instanceUrl','accessToken', 'ipType', 'ipSubtype'],)
     const ipName = ipType+'_'+ipSubtype;
     const integrationProcedureUrl = path.join(instanceUrl, process.env.SALESFORCE_INTEGRATION_PROCEDURE_URL_BASE, ipName);
-    const requestDataPath = path.join(__dirname, 'requestData.json');
+
+    console.log("1 "+integrationProcedureUrl);
+    console.log('2. ', req.body.input);
     let requestData = {};
     try {
         requestData = req.body.input;
@@ -121,13 +123,9 @@ router.post('/read-order-select-service-sample-structure', async (req, res) => {
 router.post('/invoke-order-echo-ip', async (req, res) => {
     // We know the type - everything. I see when i hit the request again and again payerConfig.json is turing in null values.
     // Understand why this is happening and fix it.
-    console.log('r1');
-    const {instanceUrl,accessToken, ipType, ipSubtype}  = fileUpdater.getFile(payerConfigFilePath,['instanceUrl','accessToken', 'ipType', 'ipSubtype'],)
-    console.log('r2'+instanceUrl+accessToken);
-    const ipName = ipType+'_'+ipSubtype;
-    alert('r3'+ipName);
+    const {instanceUrl,accessToken}  = fileUpdater.getFile(payerConfigFilePath,['instanceUrl','accessToken']);
+    const ipName = req.body.ipType+'_'+req.body.ipSubtype;
     const integrationProcedureUrl = path.join(instanceUrl, process.env.SALESFORCE_INTEGRATION_PROCEDURE_URL_BASE, ipName);
-    alert('r4'+integrationProcedureUrl);
     try {
         const response = await axios.post(integrationProcedureUrl, req.body.input, {
             headers: {
@@ -135,6 +133,7 @@ router.post('/invoke-order-echo-ip', async (req, res) => {
                 'Content-Type': 'application/json'
             }
         });
+        console.log('Got response  :', response.data);
         //send result back to oauth
         res.json({
             success: true,
@@ -142,7 +141,6 @@ router.post('/invoke-order-echo-ip', async (req, res) => {
         });
     } catch (error) {
         console.log('Error making call to Integration Procedure:', error);
-        alert('a');
         res.status(500).json({
             success: false,
             error: error.message
@@ -190,39 +188,6 @@ router.post('/useService', async (req, res) => {
         res.json({ success: true});
     })
     .catch(err => console.error('Update failed:', err));   
-});
-
-router.post('/fetchData', async (req, res) => {
-
-    const {instanceUrl,accessToken}  = fileUpdater.getFile(payerConfigFilePath,['instanceUrl','accessToken']);
-    const input = req.body;
-
-    const subApiString = 'services/data/v62.0/search?q=' + utils.makeSoslQuery(input);
-    const apiString = path.join(instanceUrl,subApiString);
-    
-    const apiUrl  = encodeURI(apiString);
-    try {
-        const response = await axios.get(apiUrl, {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            }
-        });
-        //send result back to oauth
-        res.json({
-            success: true,
-            data: response.data
-        });
-    } catch (error) {
-        console.error('Error making call SOSL', error);
-        res.status(500).json({
-            success: false,
-            error: error.message
-        });
-    }
-
-
-
 });
 
 
@@ -290,11 +255,8 @@ router.get('/payer/crdResponse', (req, res) => {
 
 router.get('/UM-Workspace', (req, res) => {
     res.render('umWorkspace',{
-        title:"UM Workspace"
+        title:"UM Workspace",
     });
-    // res.render('typeAheadUse',{
-    //     title:"UM Workspace"
-    // });
 });
 
 router.get('/getTableData', (req, res) => {
